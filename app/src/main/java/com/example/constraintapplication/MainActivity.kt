2977726c44cbcf450c0f5b0da20aa863c2b2a70c
Sushovon <sushovon.banik51@gmail.com>
2023-07-20 15:29:14 +0530

    This is my second commit

--- app/src/main/java/com/example/constraintapplication/MainActivity.kt
+++ app/src/main/java/com/example/constraintapplication/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.constraintapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getdata()
+    }
+    fun getdata(){
+        Log.d("TAG", "getdata: Github demo")
     }
 }
